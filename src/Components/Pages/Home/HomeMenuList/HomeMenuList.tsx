c490b2bc24ce0f43b15d2fcf0eee24a0819435f7
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-11-04 16:55:55 +0100

    create prop type disable on button and add new styles on buttons

--- src/Components/Pages/Home/HomeMenuList/HomeMenuList.tsx
+++ src/Components/Pages/Home/HomeMenuList/HomeMenuList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import Styles from "./HomeMenuList.module.css";
+import { ButtonPrimary, ButtonSecondary } from "../../../UI/Buttons/Buttons";
 
 function HomeMenuList() {
-  return <div className={Styles.homeMenuList}>menu list </div>;
+  return <div className={Styles.homeMenuList}>menu</div>;
 }
 
 export default HomeMenuList;
--- src/Components/UI/Buttons/Buttons.tsx
+++ src/Components/UI/Buttons/Buttons.tsx
@@ -1,15 +1,17 @@
+import { Style } from "@mui/icons-material";
 import Styles from "./Buttons.module.css";
 import React from "react";
 
 type buttonProps = {
-  children: React.ReactNode;
+  children: React.ReactNode | string;
+  type: string;
 };
 
-function ButtonPrimary({ children }: buttonProps) {
-  return <button className={Styles.buttonPrimary}>{children}</button>;
+function ButtonPrimary({ children, type }: buttonProps) {
+  return <button className={`${Styles.buttonPrimary} ${Styles[`${type}`]}`}>{children}</button>;
 }
-function ButtonSecondary({ children }: buttonProps) {
-  return <button className={Styles.buttonSecondary}>{children}</button>;
+function ButtonSecondary({ children, type }: buttonProps) {
+  return <button className={`${Styles.buttonSecondary} ${Styles[`${type}`]}`}>{children}</button>;
 }
 
 export { ButtonPrimary, ButtonSecondary };

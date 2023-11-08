bf90361222dadf980d3f6e1588fda217ba6f16cd
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-11-08 18:18:45 +0100

    ..

--- src/Components/UI/IconsLibrary/IconsLibrary.ts
+++ src/Components/UI/IconsLibrary/IconsLibrary.ts
@@ -9,16 +9,3 @@ import NavigateBeforeRoundedIcon from "@mui/icons-material/NavigateBeforeRounded
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 export { InstagramIcon, FacebookRoundedIcon, TwitterIcon, YouTubeIcon, AddRoundedIcon, RemoveRoundedIcon, NavigateNextRoundedIcon, NavigateBeforeRoundedIcon, CloseRoundedIcon };
-
-// <div className={Styles.footer}>
-//     <div className={Styles.footer_nav_section}>
-//       <p>footer nav section</p>
-//     </div>
-//     <div className={Styles.footer_promotion}>
-//       footer promotion
-//     </div>
-//     <hr />
-//     <div className={Styles.footer_legal}>
-//       footer legal
-//     </div>
-//   </div>

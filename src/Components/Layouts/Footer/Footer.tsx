09415e3f1a49a690cb05ac351e2c38bc0d0796ab
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-11-08 15:18:38 +0100

    initiate footer add IconsLibrary

--- src/Components/Layouts/Footer/Footer.tsx
+++ src/Components/Layouts/Footer/Footer.tsx
@@ -1,8 +1,41 @@
+import { Link } from "react-router-dom";
 import Styles from "./Footer.module.css";
 import React from "react";
 
+const year = new Date().getFullYear();
+
 function Footer() {
-  return <div className={Styles.footer}>footer</div>;
+  return (
+    <div className={Styles.footer}>
+      <div className={Styles.footerContainer}>
+      <div className={Styles.footer__nav_section}>
+        <p>footer nav section</p>
+      </div>
+      <div className={Styles.footer__promotion}>
+        <div className={Styles.footer__promotion_social}>
+          social media
+        </div>
+        <div className={Styles.footer__promotion_apps}>
+          <img src="src/assets//Image//footerImages//app-store-logo.png" alt="app-store-logo" />
+          <img src="src/assets//Image//footerImages//Google-Play-Logo-PNG-Photos.png" alt="google-play-logo" />
+        </div>
+      </div>
+      <hr />
+      <div className={Styles.footer__legal}>
+        <div className={Styles.footer__legal_links}>
+          <Link to="/countryselector">Country Selector</Link>
+          <Link to="/contactus">Contact us</Link>
+          <Link to="/">Privacy Policy</Link>
+          <Link to="/">Terms & Conditions</Link>
+        </div>
+        <div className={Styles.footer__legal_copyright}>
+          <img src="src/assets//Image//Icon//macdonaldPNG.png" alt="logo" />
+          <p className={Styles.copyright}>{`© ${year} McDonald's. All Rights Reserved`}</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
---
+++ src/Components/UI/IconsLibrary/IconsLibrary.ts
@@ -0,0 +1,26 @@
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+
+export {
+    InstagramIcon,
+    FacebookRoundedIcon,
+    TwitterIcon,
+    YouTubeIcon,
+
+};
+
+// <div className={Styles.footer}>
+//     <div className={Styles.footer_nav_section}>
+//       <p>footer nav section</p>
+//     </div>
+//     <div className={Styles.footer_promotion}>
+//       footer promotion
+//     </div>
+//     <hr />
+//     <div className={Styles.footer_legal}>
+//       footer legal
+//     </div>
+//   </div>

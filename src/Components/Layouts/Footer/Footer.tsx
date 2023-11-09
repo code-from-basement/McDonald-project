d7e675c9c5da4fae3b6a64f8e65bd9096821c850
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-11-09 16:34:35 +0100

    fix the footer container

--- src/Components/Layouts/Footer/Footer.tsx
+++ src/Components/Layouts/Footer/Footer.tsx
@@ -9,34 +9,79 @@ function Footer() {
   return (
     <div className={Styles.footer}>
       <div className={Styles.footerContainer}>
-      <div className={Styles.footer__nav_section}>
-        <p>footer nav section</p>
-      </div>
-      <div className={Styles.footer__promotion}>
-        <div className={Styles.footer__promotion_social}>
-          <button><FacebookRoundedIcon/></button>
-          <button><TwitterIcon/></button>
-          <button><InstagramIcon/></button>
-          <button><YouTubeIcon/></button>
+        <div className={Styles.footer__nav_section}>
+          <div className={Styles.footer__nav_box}>
+              <h2>i'm lovin' it<span>®</span></h2>  
+          </div>
+          <div className={Styles.footer__nav_box}>
+            <div className={Styles.footer__nav_box_title}>
+              <p>Our Company</p>
+              <ul>
+                <li><Link to="/">Our Company</Link></li>
+                <li><Link to="/">Our Community</Link></li>
+                <li><Link to="/">Our History</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className={Styles.footer__nav_box}>
+            <div className={Styles.footer__nav_box_title}>
+              <p>Careers</p>
+              <ul>
+                <li><Link to="/">Careers</Link></li>
+                <li><Link to="/">Training and Education</Link></li>
+                <li><Link to="/">Apply Now</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className={Styles.footer__nav_box}>
+            <div className={Styles.footer__nav_box_title}>
+              <p>Contact Us</p>
+              <ul>
+                <li><Link to="/">Contact Us</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className={Styles.footer__nav_box}>
+            <div className={Styles.footer__nav_box_title}>
+              <p>Newsroom</p>
+              <ul>
+                <li><Link to="/">Newsroom</Link></li>
+                <li><Link to="/">Local</Link></li>
+                <li><Link to="/">Regional</Link></li>
+              </ul>
+            </div>
+          </div>         
         </div>
-        <div className={Styles.footer__promotion_apps}>
-          <img src="src/assets//Image//footerImages//app-store.png" alt="app-store-logo" />
-          <img src="src/assets//Image//footerImages//google-play.png" alt="google-play-logo" />
+        <div className={Styles.footer__promotion}>
+          <div className={Styles.footer__promotion_social}>
+            <Link to=""><FacebookRoundedIcon/></Link>
+            <Link to=""><TwitterIcon/></Link>
+            <Link to=""><InstagramIcon/></Link>
+            <Link to=""><YouTubeIcon/></Link>
+          </div>
+          <div className={Styles.footer__promotion_apps}>
+            <img src="src/assets//Image//footerImages//app-store.png" alt="app-store-logo" />
+            <img src="src/assets//Image//footerImages//google-play.png" alt="google-play-logo" />
+            {/* <p>Apple and the Apple logo are trade marks of Apple Inc., registered in the U.S.
+               and other countries. App Store is a service mark of Apple Inc.,
+                registered in the U.S. and other countries. Google Play and the Google Play logo are
+                 trade marks of Google LLC.
+            </p> */}
+          </div>
         </div>
-      </div>
-      <hr />
-      <div className={Styles.footer__legal}>
-        <div className={Styles.footer__legal_links}>
-          <Link to="/countryselector">Country Selector</Link>
-          <Link to="/contactus">Contact us</Link>
-          <Link to="/">Privacy Policy</Link>
-          <Link to="/">Terms & Conditions</Link>
+        <hr />
+        <div className={Styles.footer__legal}>
+          <div className={Styles.footer__legal_links}>
+            <Link to="/countryselector">Country Selector</Link>
+            <Link to="/contactus">Contact us</Link>
+            <Link to="/">Privacy Policy</Link>
+            <Link to="/">Terms & Conditions</Link>
+          </div>
+          <div className={Styles.footer__legal_copyright}>
+            <img src="src/assets//Image//Icon//macdonaldPNG.png" alt="logo" />
+            <p className={Styles.copyright}>{`© ${year} McDonald's. All Rights Reserved`}</p>
+          </div>
         </div>
-        <div className={Styles.footer__legal_copyright}>
-          <img src="src/assets//Image//Icon//macdonaldPNG.png" alt="logo" />
-          <p className={Styles.copyright}>{`© ${year} McDonald's. All Rights Reserved`}</p>
-        </div>
-      </div>
       </div>
     </div>
   );

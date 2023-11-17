6c051f3b7b17b72b41ef0e48e5719ca7f8f3b084
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-11-17 14:08:05 +0100

    delete anime js, npm add mega menu components, still need many works

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Home from "./Components/Pages/Home/Home";
 import ShoppingCartPage from "./Components/Pages/ShoppingCartPage/ShoppingCartPage";
 import Navbar from "./Components/Layouts/Navbar/Navbar";
 import Footer from "./Components/Layouts/Footer/Footer";
+import MegaMenu from "./Components/UI/MegaMenu/MegaMenu";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Navbar />
+        <MegaMenu />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="shoppingcartpage" element={<ShoppingCartPage />} />

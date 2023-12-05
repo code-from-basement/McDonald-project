43da7393b7ab7d8ec045824d85d3d4859028b1ad
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-12-05 20:55:45 +0100

    implement Mega Menu background logo animation

--- src/Components/UI/Animation/AnimationStyles.ts
+++ src/Components/UI/Animation/AnimationStyles.ts
@@ -1,8 +1,10 @@
+import { motion } from "framer-motion";
+
 /**Mega Menu Animation Styles */
 export const megaMenuAnimationStyles = {
   key: "megaMenu",
   initial: { y: "-100%" },
-  animate: { y: 0, transition: { duration: 0.4, ease: "easeInOut" } },
-  exit: { y: "-100%", transition: { duration: 0.4, ease: "easeInOut" } },
+  animate: { y: 0, transition: { duration: 0.4, ease: "easeInOut", bounce: 20 } },
+  exit: { y: "-100%", transition: { delay: 0.2, duration: 0.4, ease: "easeInOut" } },
 };
 //
--- src/Components/UI/MegaMenu/MegaMenu.tsx
+++ src/Components/UI/MegaMenu/MegaMenu.tsx
@@ -2,11 +2,11 @@ import { useGlobalContext } from "../../Context/GlobalContext";
 import bgLogo from "./../../../assets/Image/logo/macLogo-mid-size.png";
 import { KeyboardArrowUpRoundedIcon } from "./../../UI/IconsLibrary/IconsLibrary";
 import Styles from "./MegaMenu.module.css";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { megaMenuAnimationStyles } from "../Animation/AnimationStyles";
 
 function MegaMenu() {
-  const { megaMenuToggler } = useGlobalContext();
+  const { megaMenuToggler, megaMenuOpen } = useGlobalContext();
 
   const onClickMegaMenuToggler = () => {
     megaMenuToggler();
@@ -14,9 +14,13 @@ function MegaMenu() {
 
   return (
     <motion.div {...megaMenuAnimationStyles} className={Styles.megaMenu}>
-      <div className={Styles.bg__logo}>
-        <img src={bgLogo} alt="background logo" />
-      </div>
+      <AnimatePresence>
+        {megaMenuOpen && (
+          <motion.div key="backgroundLogo" initial={{ y: "-100%" }} animate={{ y: 0, transition: { delay: 0.2 } }} exit={{ y: "300%", transition: { duration: 2 } }} className={Styles.bg__logo}>
+            <img src={bgLogo} alt="background logo" />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <section className={Styles.foodCategory}>food categories</section>
       <section className={Styles.menuAds}>menu ads</section>
       <button className={Styles.menuCloser} onClick={onClickMegaMenuToggler}>

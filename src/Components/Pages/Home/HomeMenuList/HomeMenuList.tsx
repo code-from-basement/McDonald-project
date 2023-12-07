e98aedb05091a0d96759580db65b0cedccf18308
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-12-07 22:50:13 +0100

    implement main menu categories, still need a lot of impotent

--- src/Components/Pages/Home/HomeMenuList/HomeMenuList.tsx
+++ src/Components/Pages/Home/HomeMenuList/HomeMenuList.tsx
@@ -1,8 +1,37 @@
 import Styles from "./HomeMenuList.module.css";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper/modules";
+import { PaginationItem } from "@mui/material";
 
 function HomeMenuList() {
-  return <div className={Styles.homeMenuList}>menu</div>;
+  return (
+    <div className={Styles.homeMenuList}>
+      <Swiper
+        slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{
+          type: "progressbar",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className={Styles.menuListContainer}
+      >
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+        <SwiperSlide className={Styles.categoryItem}>Slide 1</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default HomeMenuList;
--- src/Components/UI/Animation/AnimationStyles.ts
+++ src/Components/UI/Animation/AnimationStyles.ts
@@ -5,7 +5,7 @@ export const megaMenuAnimationStyles = {
   key: "megaMenu",
   initial: { y: "-100%" },
   animate: { y: 0, transition: { duration: 0.4, ease: "easeInOut", bounce: 20 } },
-  exit: { y: "-100%", transition: { delay: 0.2, duration: 0.4, ease: "easeInOut" } },
+  exit: { y: "-100%", transition: { delay: 0.3, duration: 0.4, ease: "easeInOut" } },
 };
 //
 
@@ -13,5 +13,5 @@ export const megaMenuBackgroundLogoAnimationStyles = {
   key: "backgroundLogo",
   initial: { y: "-100%" },
   animate: { y: 0, transition: { delay: 0.2 } },
-  exit: { y: "300%", transition: { duration: 2 } },
+  exit: { y: "100%", transition: { delay: 0.1, duration: 1.2 } },
 };

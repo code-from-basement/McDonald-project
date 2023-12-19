edae8e4d78069e7374dc535f49b3c82e1ce85c7e
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-12-19 13:52:15 +0100

    ...

--- src/Components/Pages/AboutOurFood/AboutOurFood.tsx
+++ src/Components/Pages/AboutOurFood/AboutOurFood.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Styles from './AboutOurFood.module.css'
 import banner from '../../../assets/Image/banner/mcd-sv-core-omvarmat-pommesfrites-2col.jpeg'
-import background from '../../../assets/Image/panelWrapper/PaperBagTexture_1.jpg'
 
 function AboutOurFood() {
   return (
@@ -13,15 +12,6 @@ function AboutOurFood() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, cum dolores placeat itaque dolor quasi eaque quis labore sint nam veniam cupiditate at, reprehenderit voluptatibus a optio temporibus neque! Hic totam voluptatem quasi cum dolorem iusto similique libero dignissimos. Laboriosam enim sequi dolor dicta, explicabo distinctio consequatur, nesciunt repellat necessitatibus minus, quisquam officia ex iste ut ratione debitis cumque rerum voluptates fugit unde? Nisi laboriosam blanditiis expedita voluptatem magni est, voluptates voluptate doloribus libero! In, veniam sunt! Consectetur distinctio quasi, assumenda nemo odit explicabo eaque eum ex a recusandae ea. Quam quisquam eveniet eaque necessitatibus aspernatur quaerat. Hic magni sint laborum natus rem fugiat expedita quos quasi consequatur earum architecto nesciunt, iusto labore, unde eos recusandae quis deleniti eum magnam officiis dolores? Officia velit, nisi corporis perspiciatis in, at temporibus doloribus, suscipit quisquam explicabo dolore accusantium! Aut, modi molestias recusandae pariatur, quis reiciendis excepturi voluptatem fugiat dignissimos minima odio quo.</p>
       </div>
       <div className={Styles.panelWrapper}>
-
-        {/* <img src={background} alt='PaperBag Texture with the macDonald logo'/> */}
-          {/* <div className={Styles.imageContainer}>
-            hello
-          </div>
-          <div className={Styles.cardContainer}>
-
-          </div> */}
-
       </div>
     </div>
   )

75c9a5f0645514ecd8587f833a8dfcecfbcc0ab4
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-01-05 10:23:12 +0100

    adding react countUp in aboutOurFood page

--- src/Components/Pages/AboutOurFood/AboutOurFood.tsx
+++ src/Components/Pages/AboutOurFood/AboutOurFood.tsx
@@ -5,8 +5,19 @@ import iceCreamImg from "../../../assets/Image/panelWrapper/230329_Limehouse_DDB
 import imageOfWomen from "../../../assets/Image/panelWrapper/Jobs & inclusion3.png";
 import imageOfBoy from "../../../assets/Image/panelWrapper/Community connection3_0.png";
 import { motion } from "framer-motion";
+import CountUp from "react-countup"
 
 function AboutOurFood() {
+
+  const [loading, setLoading] = React.useState(false);
+  const onStart = () => {setLoading(true)};
+  const onEnd = () => {setLoading(false)};
+  const containerProps = {
+    'aria-busy': loading
+  };
+
+
+
   return (
     <div className={Styles.aboutOurFood}>
       {/* <div className={Styles.bannerContainer}>
@@ -14,14 +25,36 @@ function AboutOurFood() {
       </div> */}
       <div className={Styles.banner}></div>
       <div className={Styles.detailsContainer}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, cum dolores placeat itaque dolor quasi eaque quis labore sint nam veniam cupiditate at, reprehenderit voluptatibus a optio temporibus neque! Hic totam voluptatem
-          quasi cum dolorem iusto similique libero dignissimos. Laboriosam enim sequi dolor dicta, explicabo distinctio consequatur, nesciunt repellat necessitatibus minus, quisquam officia ex iste ut ratione debitis cumque rerum voluptates
-          fugit unde? Nisi laboriosam blanditiis expedita voluptatem magni est, voluptates voluptate doloribus libero! In, veniam sunt! Consectetur distinctio quasi, assumenda nemo odit explicabo eaque eum ex a recusandae ea. Quam quisquam
-          eveniet eaque necessitatibus aspernatur quaerat. Hic magni sint laborum natus rem fugiat expedita quos quasi consequatur earum architecto nesciunt, iusto labore, unde eos recusandae quis deleniti eum magnam officiis dolores?
-          Officia velit, nisi corporis perspiciatis in, at temporibus doloribus, suscipit quisquam explicabo dolore accusantium! Aut, modi molestias recusandae pariatur, quis reiciendis excepturi voluptatem fugiat dignissimos minima odio
-          quo.
-        </p>
+        <div className={Styles.detailsContainer_counter}>
+          <h2>Our Impact</h2>
+          <div className={Styles.counter}>
+            <div className={Styles.counter_items}>
+              <p>Restaurant</p>
+              <CountUp onStart={onStart} onEnd={onEnd} containerProps={containerProps}  end={1020} duration={3} suffix="+" />
+            </div>
+            <div className={Styles.counter_items}>
+              <p>Licensees</p>
+              <CountUp onStart={onStart} onEnd={onEnd} containerProps={containerProps}  end={200} duration={3.6} delay={0.1} suffix="+" />
+            </div>
+            <div className={Styles.counter_items}>
+              <p>Employees</p>
+              <CountUp onStart={onStart} onEnd={onEnd} containerProps={containerProps}  end={110000} duration={3} delay={0.2} suffix="+" />
+            </div>
+          </div>          
+        </div>
+        <div className={Styles.detailsContainer_content}>
+          <p>
+            We’ve been a part of Australian communities for more than 50 years, serving great quality,
+            great value food and creating feel-good Macca’s moments for our people, customers and communities.
+          </p>
+          <p>
+            We know we have a responsibility and opportunity to drive positive outcomes from the farm to the front counter and beyond,
+            in the areas that matter most to our customers, employees, suppliers, franchisees and communities.
+          </p>
+          <p>
+            Changing for the better? We’re On It.
+          </p>
+        </div>
       </div>
       <div className={Styles.panelWrapper}>
         <div className={Styles.row1}>

e13b61c89ec1fd46cff97f121bc24a617e0e1271
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-01-12 16:38:40 +0100

    add loose container to TrendingNow component

--- src/Components/Pages/TrenedingNow/TrendingNow.tsx
+++ src/Components/Pages/TrenedingNow/TrendingNow.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Styles from './TrendingNow.module.css'
 import deluxe from "../../../assets/Image/Trending/otherPic/Trending-Aussie-Angus-Deluxe-Right-2024-0110.png"
 import macCrispy from '../../../assets/Image/Trending/otherPic/Trending-McCrispy-Bacon-Deluxe-Left-2023-1113.png'
+import macSauce from "../../../assets/Image/Trending/otherPic/big-mac-sauce-uae-en.png"
+import macJr from "../../../assets/Image/Trending/otherPic/mac-jr-en-uae.png"
+import grandMac from "../../../assets/Image/Trending/otherPic/grand-mac-en-uae.png"
+
+
+
 
 function TrendingNow() {
   return (
@@ -22,7 +28,35 @@ function TrendingNow() {
           <div className={Styles.macContainer_link} >
             <a href="#">Learn more</a>
           </div>
-
+        </div>        
+      </div>
+      <div className={Styles.looseMenu}>
+        <div className={Styles.looseMenu_container}>
+          <h3>Loose Change Menu</h3>
+          <div className={Styles.looseMenu_container__boxes}>
+            <div className={Styles.smallBox}>
+              <img src={macSauce} alt='A pic of big mac sauce.'/>
+              <h3>App Exclusive</h3>
+              <p>The iconic taste is now available as a dip, the new limited time Big Mac Sauce! Add it to your order only on the McDonald’s App. </p>
+              {/* <button>click here</button> */}
+            </div>
+            <div className={Styles.smallBox}>
+              <img src={grandMac} alt='A pic of grand Mac.'/>
+              <h3>App Exclusive</h3>
+              <p>More beef, more cheese, more sauce… Order the new limited time Grand Big Mac! </p>
+              {/* <button>click here</button> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={Styles.looseMenuDown}>
+        <div className={Styles.looseMenu_container}>
+            <div className={Styles.smallBox}>
+              <img src={macJr} alt='A pic of mac Jr.'/>
+              <h3>App Exclusive</h3>
+              <p>All the flavor… one less layer! Introducing the new limited time Mac Jr. </p>
+              {/* <button>click here</button> */}
+            </div>
         </div>
       </div>
     </div>

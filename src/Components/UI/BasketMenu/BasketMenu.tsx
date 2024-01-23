10611012fc03f21587d09bb37b9fd9ca0cdb8fb4
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-01-23 19:29:32 +0100

    bug fix in basket slider styles

--- src/Components/UI/BasketMenu/BasketMenu.tsx
+++ src/Components/UI/BasketMenu/BasketMenu.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import Styles from "./BasketMenu.module.css";
 import { motion } from "framer-motion";
+import { useGlobalContext } from "../../Context/GlobalContext";
 import { basketMenuAnimationStyles } from "../Animation/AnimationStyles";
 import { NavigateNextRoundedIcon } from "../IconsLibrary/IconsLibrary";
+import Styles from "./BasketMenu.module.css";
 import EmptyCart from "./EmptyCart/EmptyCart";
-import { useGlobalContext } from "../../Context/GlobalContext";
+
 function BasketMenu() {
   const { eventToggles } = useGlobalContext();
   const { togglerFunc, isBasketEmpty } = eventToggles;
+
   return (
     <motion.div {...basketMenuAnimationStyles} className={Styles.basketMenu}>
       <button onClick={() => togglerFunc("isBasketShow", !eventToggles.isBasketShow)} className={Styles.btn__close}>

10f68feb2a17b4270c1233e99326b2adf34d7090
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-01-28 22:16:34 +0100

    ..

--- src/Components/UI/MenuItem/MenuItem.tsx
+++ src/Components/UI/MenuItem/MenuItem.tsx
@@ -17,6 +17,7 @@ function MenuItem({ item }: any) {
             </p>
           </div>
           <div className={Styles.header__btn}>
+            <h2>{price}Kr</h2>
             <button>
               <FavoriteBorderRoundedIcon />
             </button>

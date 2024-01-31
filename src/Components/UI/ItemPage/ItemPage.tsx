104b7a462acf91d4063472c87430bf82170583b6
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-01-31 20:31:14 +0100

    bug in page item , durring go through and render nutritions data

--- src/Components/UI/ItemPage/ItemPage.tsx
+++ src/Components/UI/ItemPage/ItemPage.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import { useGlobalContext } from "../../Context/GlobalContext";
 import macIcon from "./../../../assets/Image/Icon/macdonaldPNG.png";
 import Styles from "./itemPage.module.css";
+import { selectClasses } from "@mui/material";
 
 function ItemPage() {
   const { fullMenuListData, isLoading, setIsLoading, fetchAllMenuData } = useGlobalContext();
   const [selectedProduct, setSelectedProduct] = useState();
-
   const { title } = useParams();
-  // To reloacte to the top of the page when the page is loaded
+  const ingredientString = selectedProduct?.ingredient.toString().replace(/,/g, ", ") + ".";
+
+  // nutrition list component , extract the object and map it to a list
+  const nutritionList =
+    fullMenuListData &&
+    Object.entries(selectedProduct?.nutrition).map((item, index) => {
+      return (
+        <li className={Styles.listItem} key={index}>
+          <h2>
+            {item[0].charAt(0).toUpperCase() + item[0].slice(1).toLowerCase()}: {item[1]}
+          </h2>
+        </li>
+      );
+    });
+
+  // To relocate to the top of the page when the page is loaded
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -40,20 +55,22 @@ function ItemPage() {
         </section>
         <section className={Styles.section__content}>
           <div className={Styles.item__card}>
-            <h2 className={Styles.title}>{selectedProduct?.title}</h2>
-            <h2 className={Styles.price}>{selectedProduct?.price}Kr</h2>
-            <h2 className={Styles.description}>{selectedProduct?.description}</h2>
+            <h2 className={Styles.item__title}>{selectedProduct?.title}</h2>
+            <h2 className={Styles.item__price}>{selectedProduct?.price}Kr</h2>
+            <h2 className={Styles.item__description}>{selectedProduct?.description}</h2>
             <span className={Styles.TagCalory}>
               {selectedProduct?.nutrition.calories} <br /> Cal.
             </span>
           </div>
           <div className={Styles.ingredient__card}>
-            <h2>{selectedProduct?.title}</h2>
-            <h2>{selectedProduct?.price}</h2>
+            <h2 className={Styles.ingredient__title}>Ingredients: </h2>
+            <h2 className={Styles.ingredient__text}>{ingredientString}</h2>
           </div>
-          <div className={Styles.Nutrition__card}>
-            <h2>{selectedProduct?.title}</h2>
-            <h2>{selectedProduct?.price}Kr</h2>
+          <div className={Styles.nutrition__card}>
+            <h2 className={Styles.nutrition__title}>Nutrition:</h2>
+            <div className={Styles.nutrition__list}>
+              <ul className={Styles.list}>{nutritionList}</ul>
+            </div>
           </div>
         </section>
       </section>

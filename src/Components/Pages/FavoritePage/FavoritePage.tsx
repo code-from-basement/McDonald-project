7abee3e9b556c40624a41ec974f97f0d27dee488
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-02-15 13:10:01 +0100

    working on favorite page

--- src/Components/Pages/FavoritePage/FavoritePage.tsx
+++ src/Components/Pages/FavoritePage/FavoritePage.tsx
@@ -15,11 +15,40 @@ function FavoritePage() {
     }
   }, []);
   console.log(userFavoriteList);
+  const noFavItem = <h2>There no favorite item selected</h2>;
 
   return (
     <div className={Styles.favPage}>
-      FavoritePage
-      <h2>{dataBase.currentUser?.displayName} favorite menu:</h2>
+      <section className={Styles.favPage__header}>
+        <h1 className={Styles.header__title}>Favorite</h1>
+        <h2 className={Styles.header__subTitle}>
+          {dataBase.currentUser?.displayName} <br /> favorite menu:
+        </h2>
+      </section>
+      <section className={Styles.favPage__filterPanel}>
+        <div className={Styles.filterPanel__container}>
+          <button className={Styles.filterPanel__btn}>Cheapest</button>
+          <button className={Styles.filterPanel__btn}>Most expensive</button>
+          <button className={Styles.filterPanel__btn}>Fat</button>
+          <button className={Styles.filterPanel__btn}>Calories</button>
+          <button className={Styles.filterPanel__btn}>Categories</button>
+        </div>
+      </section>
+      <section className={Styles.favPage__favList}>
+        <div className={Styles.favList__container}>
+          {!dataBase.currentUser
+            ? noFavItem
+            : userFavoriteList?.map((item: any) => {
+                const { title, price } = item;
+                return (
+                  <div>
+                    <h2>{title}</h2>
+                    <h2>{price}</h2>
+                  </div>
+                );
+              })}
+        </div>
+      </section>
     </div>
   );
 }

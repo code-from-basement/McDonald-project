60aba0fbf1f5a2c64ecc39f6dc862abbaa4b5580
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-09 16:55:36 +0200

    add logic on loading to be center durring be in view
    add logic on sticky basket on on click jump on top and open basket mene in 1000 ms

--- src/Components/UI/BasketSticky/BasketSticky.tsx
+++ src/Components/UI/BasketSticky/BasketSticky.tsx
@@ -6,13 +6,27 @@ import { LocalMallOutlinedIcon } from "../IconsLibrary/IconsLibrary";
 import Styles from "./BasketSticky.module.css";
 
 function BasketSticky() {
-  const { basketList } = useGlobalContext();
+  const { basketList, eventToggles, setEventToggles } = useGlobalContext();
+  const { isBasketShow } = eventToggles;
   const location = useLocation();
   const isLogin = location.pathname === "/login";
+
+  const onClickStickyBasketHandler = () => {
+    document.querySelector("#navbar")?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    setTimeout(() => {
+      setEventToggles((prevData) => {
+        return { ...prevData, isBasketShow: true };
+      });
+    }, 1000);
+  };
+
   if (isLogin) return null;
 
   return (
-    <motion.div {...stickyBasketANimationStyles} className={Styles.basketSticky}>
+    <motion.div onClick={onClickStickyBasketHandler} {...stickyBasketANimationStyles} className={Styles.basketSticky}>
       <LocalMallOutlinedIcon />
       <div className={Styles.divider}>&nbsp;</div>
       <h2>{basketList.length}</h2>
--- src/Components/UI/MainMenuLoading/MainMenuLoading.tsx
+++ src/Components/UI/MainMenuLoading/MainMenuLoading.tsx
@@ -1,10 +1,18 @@
 import { motion } from "framer-motion";
 import loadingGift from "../../../assets/UI/yrtk-loader.gif";
 import Styles from "./MainMenuLoading.module.css";
+import { useEffect } from "react";
 
 function MainMenuLoading() {
+  useEffect(() => {
+    document.getElementById("mainMenuLoadingID")?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "center",
+    });
+  }, []);
   return (
-    <motion.div className={Styles.layout} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0, transition: { delay: 1 } }}>
+    <motion.div id="mainMenuLoadingID" className={Styles.layout} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0, transition: { delay: 1 } }}>
       <img src={loadingGift} alt="loading gif image" />
     </motion.div>
   );

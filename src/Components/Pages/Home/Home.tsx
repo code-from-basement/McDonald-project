3ed65d64997732cd2beb45268757030e03ac874d
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-13 16:42:58 +0200

    still need to fix reload problem

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import FQA from "./FQA/FQA";
 import Header from "./Header/Header";
 import Styles from "./Home.module.css";
@@ -7,22 +5,6 @@ import HomeMenuList from "./HomeMenuList/HomeMenuList";
 import MenuContainer from "./MenuContainer/MenuContainer";
 
 function Home() {
-  const pathName = useLocation();
-  console.log(pathName);
-
-  useEffect(() => {
-    const reloadFunc = () => {
-      console.log("-----------------------------");
-      pathName.pathname == "/";
-      window.location.href = pathName.pathname;
-    };
-
-    window.addEventListener("beforeunload", reloadFunc);
-    return () => {
-      window.removeEventListener("beforeunload", reloadFunc);
-    };
-  }, []);
-
   return (
     <div className={Styles.home}>
       <Header />
--- src/Components/Pages/Home/MenuContainer/AllMenu/AllMenu.tsx
+++ src/Components/Pages/Home/MenuContainer/AllMenu/AllMenu.tsx
@@ -3,6 +3,7 @@ import MenuItem from "../../../../UI/MenuItem/MenuItem";
 import Styles from "./AllMenu.module.css";
 function AllMenu() {
   const { fullMenuListData }: any = useGlobalContext();
+
   return (
     <div className={Styles.allMenuContainer}>
       <h3 className={Styles.title}>All menu</h3>
